dedf093fc1d76e12f5ad8a543f43a1963ec3f36f
Aditya Gattu <gattuaditya@gmail.com>
2024-03-29 13:38:52 -0600

    Updated Styling in pages

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -40,7 +40,7 @@ const placeOrder = async (req, res) => {
         
           const session = await stripe.checkout.sessions.create({
             success_url: `https://fooddeliveryapp99.netlify.app/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `https://fooddeliveryapp99.netlify.app//verify?success=false&orderId=${newOrder._id}`,
+            cancel_url: `https://fooddeliveryapp99.netlify.app/verify?success=false&orderId=${newOrder._id}`,
             line_items: line_items,
             mode: 'payment',
           });
